04fd9d07d50681a0e05915fb3a9a935e5a172a37
Drew Nguyen <drew.ktran@gmail.com>
2019-06-19 16:16:33 -0700

    updated user model

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,7 @@
 const pool = require("../pool");
 userModel = {
   // add a product to products database
+  // json body: { "u_id":"value", "prod_name":"value", "prod_desc":"value" }
   newProduct(body) {
     const newProductQuery = `INSERT INTO "public"."products" (u_id,prod_name,prod_desc,prod_price,img_url,stock,zipcode,location)
         VALUES ('${body.u_id}','${body.prod_name}','${body.prod_desc}','${
@@ -15,6 +16,7 @@ userModel = {
   },
   // add a new user to the "users" table
   // body payload: { "u_id":"value", "name":"r kim", "img_url":"value", "email":"value" }
+  // used for adding a new user to database
   newUser(body) {
     const newUserQuery = `INSERT INTO public.users (user_id,name,usr_pic,email)
         VALUES ('${body.u_id}','${body.name}','${body.img_url}','${
@@ -22,6 +24,20 @@ userModel = {
     }')`;
     return new Promise((resolve, reject) => {
       pool.query(newUserQuery, (err, result) => {
+        if (err) return reject(err);
+        resolve(result);
+      });
+    });
+  },
+
+  // returning user's products
+  // body json should have at least: { "u_id":"value" }
+  getMyProducts(body) {
+    const myProductsQuery = `SELECT * FROM products INNER JOIN users ON users.user_id = products.u_id WHERE users.user_id=${
+      body.u_id
+    }`;
+    return new Promise((resolve, reject) => {
+      pool.query(myProductsQuery, (err, result) => {
         if (err) return reject(err);
         resolve(result);
       });
