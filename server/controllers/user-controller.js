3cbcef05dc9ef7334f4f479d7bf190535d555a3d
Drew Nguyen <drew.ktran@gmail.com>
2019-06-19 16:46:10 -0700

    tested queries in user model

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,11 +1,17 @@
 const User = require('../models/user');
 
 const userCtrl = {};
-
+/**
+* getAllProducts - returns all products
+* @param req - http.IncomingRequest
+* @param res - http.ServerResponse
+* @param next - Callback Function w signature (err, users)
+*/
 // middleware to handle creating a new user, and adding to "users" table
-
+userCtrl.createNewUser = (req, res) => {};
 // middleware to return only the user's items for crud operations
-
+userCtrl.getMyProducts = (req, res) => {};
 // middleware to allow users to add products with corresponding u_id
+userCtrl.addNewProduct = (req, res) => {};
 
 module.exports = userCtrl;
