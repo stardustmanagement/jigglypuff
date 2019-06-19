c74c61d0792092b3eb746b48f624c046c5ea6d04
Rachel Kim <rayykim323@gmail.com>
2019-06-19 12:41:13 -0700

    passport

--- server/server.js
+++ server/server.js
@@ -1,31 +1,55 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const routes = require('./routes/api');
+const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
+const path = require("path");
+const bodyParser = require("body-parser");
+const routes = require("./routes/api");
 const { PORT } = process.env;
 const app = express();
 
+// creates new instance of Google Strategy
+// generic register for passport.use
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Static route to access images hosted in server
-app.use('/static', express.static(path.join(__dirname, 'public')))
-app.use('/api', routes);
+app.use("/static", express.static(path.join(__dirname, "public")));
+app.use("/api", routes);
 
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   //404
-  res.locals.message = 'PAGE NOT FOUND';
-  const err = new Error('RESOURCE NOT FOUND');
+  res.locals.message = "PAGE NOT FOUND";
+  const err = new Error("RESOURCE NOT FOUND");
   err.status = 404;
   return next(err);
 });
 
 // Dedicated error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   res.status(404).json(err);
 });
 
-app.listen(PORT, () => {
+app.listen(3000, () => {
   console.log(`server listening on port ${PORT}`);
 });
