0deb7b144e8bf55a3bf14c345d90816413fe255c
Rachel Kim <rayykim323@gmail.com>
2019-06-19 19:31:48 -0700

    oauth user id

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
   googleClientID:
     "151774167807-npvkk2587fqs9vqc4pdo9eph5a2btg1h.apps.googleusercontent.com",
-  googleClientSecret: "mSk98_bRRrXG2gKfbL0UzCvD"
+  googleClientSecret: "mSk98_bRRrXG2gKfbL0UzCvD",
+  cookieKey: "fsfasfasfasfsfafafaf"
 };
