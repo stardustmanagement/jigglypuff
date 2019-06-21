b3802be641101e589860563ace00669ea67dfce4
Dean Ohashi <dnohashi@Deans-MacBook-Pro-2.local>
2019-06-20 17:53:39 -0700

    fixed app.jsx

--- client/containers/App.jsx
+++ client/containers/App.jsx
@@ -59,29 +59,6 @@ const mapStateToProps = store => ({
   onCheckoutPage: store.products.onCheckoutPage
 });
 
-//wire-up Router in here
-function App({ onCheckoutPage }) {
-  return (
-    <Router>
-      <div>
-        <Header />
-        <div className="tabs">
-          <Link to="/addproduct" className="addproduct">
-            Add Product
-          </Link>
-          <Link to="/myproducts" className="myproduct">My Products</Link>
-        </div>
-        {onCheckoutPage && <PurchaseModal />}
-
-        <Route exact path="/" component={MainDisplay} />
-        <Route path="/addproduct" component={AddProduct} />
-        <Route path="/myproducts" component={UserCatalog} />
-
-  
-      </div>
-    </Router>
-  );
-}
 const mapDispatchToProps = dispatch => ({
   addUser: userId => dispatch(actions.addUser(userId))
 })
